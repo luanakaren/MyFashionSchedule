12fe83f51107b07fdf0240af9a0af7ec068ef5a7
Karen <l_a_karen@hotmail.fr>
2016-09-06 10:40:48 +0300

    Données pour la marque Adidas

--- src/databaseScript/insert2.sql
+++ src/databaseScript/insert2.sql
@@ -1,30 +1,220 @@
 /* PICTURE */
 INSERT INTO picture(link)
-    VALUES ('http://placehold.it/650x450&text=Profil');
+    VALUES ('http://localhost:8282/assets/img/clothes/AE9412.jpg');
 
 INSERT INTO picture(link)
-    VALUES ('http://placehold.it/650x450&text=Clothing 1');
+    VALUES ('http://localhost:8282/assets/img/clothes/AE9414.jpg');
 
 INSERT INTO picture(link)
-    VALUES ('http://placehold.it/650x450&text=Clothing 2');
+    VALUES ('http://localhost:8282/assets/img/clothes/AE9405.jpg');
 
 INSERT INTO picture(link)
-    VALUES ('http://placehold.it/650x450&text=Clothing 3');
+    VALUES ('http://localhost:8282/assets/img/clothes/BQ0999.jpg');
 
 INSERT INTO picture(link)
-    VALUES ('http://placehold.it/650x450&text=Clothing 4');
+    VALUES ('http://localhost:8282/assets/img/clothes/BQ1010.jpg');
 
 INSERT INTO picture(link)
-    VALUES ('http://placehold.it/650x450&text=Clothing 5');
+    VALUES ('http://localhost:8282/assets/img/clothes/AY9005.jpg');
 
 INSERT INTO picture(link)
-    VALUES ('http://placehold.it/650x450&text=Clothing 6');
+    VALUES ('http://localhost:8282/assets/img/clothes/AX5469.jpg');
 
 INSERT INTO picture(link)
-    VALUES ('http://placehold.it/650x450&text=Clothing 7');
+    VALUES ('http://localhost:8282/assets/img/clothes/BQ0987.jpg');
 
 INSERT INTO picture(link)
-    VALUES ('http://placehold.it/650x450&text=Clothing 8');
+    VALUES ('http://localhost:8282/assets/img/clothes/S97423.jpg');
+
+INSERT INTO picture(link)
+    VALUES ('http://localhost:8282/assets/img/clothes/AZ3240.jpg');
+
+INSERT INTO picture(link)
+    VALUES ('http://localhost:8282/assets/img/clothes/BB1970.jpg');
+
+INSERT INTO picture(link)
+    VALUES ('http://localhost:8282/assets/img/clothes/S75910.jpg');
+
+/* CLOTHING */
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand,
+            name_clothing, reference_clothing, link_officialwebsite)
+    VALUES (8, 2, 1, 1,
+            'Key printed short', 'AE9412', 'http://www.global.adidas.com/key-printed-short/AE9412.html');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand,
+            name_clothing, reference_clothing, link_officialwebsite)
+    VALUES (8, 2, 2, 1,
+            'Key printed short', 'AE9414', 'http://www.global.adidas.com/key-printed-short/AE9412.html');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand,
+            name_clothing, reference_clothing, link_officialwebsite)
+    VALUES (6, 2, 3, 1,
+            'Pull on ankle pant', 'AE9405', 'http://www.global.adidas.com/pull-on-ankle-pant/AE9405.html');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand,
+            name_clothing, reference_clothing, link_officialwebsite)
+    VALUES (5, 2, 4, 1,
+            'Blue geology crew sweat', 'BQ0999', 'http://www.global.adidas.com/blue-geology-crew-sweat/BQ0999.html');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand,
+            name_clothing, reference_clothing, link_officialwebsite)
+    VALUES (5, 2, 5, 1,
+            'Blue geology oversized hoody', 'BQ1010', 'http://www.global.adidas.com/blue-geology-oversized-hoody/BQ1010.html');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand,
+            name_clothing, reference_clothing, link_officialwebsite)
+    VALUES (5, 2, 6, 1,
+            'Crop hoodie', 'AY9005', 'http://www.global.adidas.com/crop-hoodie/AY9005.html');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand,
+            name_clothing, reference_clothing, link_officialwebsite)
+    VALUES (6, 2, 7, 1,
+            '3-Stripes tapered pants', 'AX5469', 'http://www.global.adidas.com/3-stripes-tapered-pants/AX5469.html');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand,
+            name_clothing, reference_clothing, link_officialwebsite)
+    VALUES (2, 2, 9, 1,
+            'Allover print dress', 'S97423', 'http://www.global.adidas.com/allover-print-dress/S97423.html');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand,
+            name_clothing, reference_clothing, link_officialwebsite)
+    VALUES (2, 2, 10, 1,
+            'Floral burst tank top dress', 'AZ3240', 'http://www.global.adidas.com/floral-burst-tank-top-dress/AZ3240.html');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand,
+            name_clothing, reference_clothing, link_officialwebsite)
+    VALUES (25, 2, 11, 1,
+            'NMD_R1 shoes', 'BB1970', 'http://www.global.adidas.com/nmd_r1-shoes/BB1970.html');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand,
+            name_clothing, reference_clothing, link_officialwebsite)
+    VALUES (25, 2, 12, 1,
+            'Tubural viral shoes', 'S75910', 'http://www.global.adidas.com/tubular-viral-shoes/S75910.html');
+
+/* CLOTHING_SEASONS */
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (1, 1);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (1, 2);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (1, 3);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (3, 3);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (1, 4);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (2, 4);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (3, 4);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (4, 4);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (2, 5);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (3, 5);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (4, 5);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (2, 6);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (3, 6);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (4, 6);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (1, 7);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (2, 7);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (3, 7);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (4, 7);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (1, 8);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (3, 8);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (4, 8);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (1, 9);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (2, 9);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (3, 9);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (4, 9);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (1, 10);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (3, 10);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (1, 11);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (2, 11);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (3, 11);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (4, 11);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (1, 12);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (2, 12);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (3, 12);
+
+INSERT INTO clothing_seasons(id_season, id_clothing)
+    VALUES (4, 12);
+
+/* CLOTHES */
+INSERT INTO clothes(id_clothing, id_dressing)
+    VALUES (1, 1);
+
+INSERT INTO clothes(id_clothing, id_dressing)
+    VALUES (4, 1);
+
+INSERT INTO clothes(id_clothing, id_dressing)
+    VALUES (7, 1);
+
+INSERT INTO clothes(id_clothing, id_dressing)
+    VALUES (9, 1);
+
+INSERT INTO clothes(id_clothing, id_dressing)
+    VALUES (10, 1);
+
+INSERT INTO clothes(id_clothing, id_dressing)
+    VALUES (11, 1);
 
 /* GENDER */
 INSERT INTO public.gender(name_gender)
@@ -57,15 +247,4 @@ INSERT INTO public.brand(name_brand)
 
 INSERT INTO public.brand(name_brand)
     VALUES ('Calvin Klein');
-
-/* CLOTHING */
-INSERT INTO public.clothing(id_brand, id_subcategory, id_picture,
-            name_clothing, reference_clothing, link_officialwebsite, id_gender)
-    VALUES (7, 5, 2,
-            'Blouse - ecru', 'C3121E00F-A11', 'https://www.zalando.fr/cacharel-blouse-ecru-c3121e00f-a11.html', 2);
-
-INSERT INTO public.clothing(id_brand, id_subcategory, id_picture,
-            name_clothing, reference_clothing, link_officialwebsite, id_gender)
-    VALUES (4, 4, 3,
-            'ISLA - Pullover - purple', 'TO121I04C-G11', 'https://www.zalando.fr/tommy-hilfiger-isla-pullover-purple-to121i04c-g11.html', 2);
 
---
+++ web/add-clothes.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Haja
+  Date: 01/09/2016
+  Time: 19:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
