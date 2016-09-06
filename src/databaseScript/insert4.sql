848ca3b3ffd4215b97b7c2affa8f8a69c5723cc3
Karen <l_a_karen@hotmail.fr>
2016-09-06 15:13:49 +0300

    Données de la marque Mango

---
+++ src/databaseScript/insert4.sql
@@ -0,0 +1,172 @@
+/********* MANGO *********/
+
+/* PICTURE */
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73015577.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73097519.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73070134.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73015564.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73003541.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73080131.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73010132.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73033531.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73095535.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73040138.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73060083.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73085594.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73025581.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/71036520.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/71045544.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73073511.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73085560.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73060085.jpg');
+INSERT INTO picture(link) VALUES ('http://localhost:8282/assets/img/clothes/73093524.jpg');
+
+/* CLOTHING */
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (19, 2, 13, 6, 'Circular embellishment necklace', 'REF. 73015577 - DISCO', 'http://shop.mango.com/US/p0/women/accessories/jewellery/necklaces/circular-embellishment-necklace/?id=73015577_OR&n=1&s=accesorios.bisuteria');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (19, 2, 14, 6, '2 velvet chockers pack', 'REF. 73097519 - JOALE', 'http://shop.mango.com/US/p0/women/accessories/jewellery/necklaces/2-velvelt-chockers-pack/?id=73097519_51&n=1&s=accesorios.bisuteria');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (18, 2, 15, 6, 'Cross-body small bag', 'REF. 73070134 - COLETAS', 'http://shop.mango.com/US/p0/women/accessories/bags/crossbody-bags/cross-body-small-bag/?id=73070134_99&n=0&ts=1473148739136');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (19, 2, 16, 6, 'Bead chain necklace', 'REF. 73015564 - DINGO', 'http://shop.mango.com/US/p0/women/accessories/jewellery/necklaces/bead-chain-necklace/?id=73015564_PL&n=1&s=accesorios.bisuteria');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (20, 2, 17, 6, 'Hoop earings', 'REF. 73003541 - INDUS', 'http://shop.mango.com/US/p0/women/accessories/jewellery/earrings/hoop-earrings/?id=73003541_PL&n=0&ts=1473150180760');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (18, 2, 18, 6, 'Chain bag', 'REF. 73080131 - MOS7', 'http://shop.mango.com/US/p0/women/accessories/bags/crossbody-bags/chain-bag/?id=73080131_01&n=0&ts=1473150180760');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (13, 2, 19, 6, 'Die-cut design shoes', 'REF. 73010132 - CUTY1', 'http://shop.mango.com/US/p0/women/accessories/shoes/flat-shoes/die-cut-design-shoes/?id=73010132_15&n=0&ts=1473151620958');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (21, 2, 20, 6, 'Rigid bracelet set', 'REF. 73033531 - INDUS', 'http://shop.mango.com/US/p0/women/accessories/jewellery/bracelets/rigid-bracelet-set/?id=73033531_PL&n=1&s=accesorios.bisuteria');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (10, 2, 21, 6, 'Check wool-blend coat', 'REF. 73095535 - CHESS', 'http://shop.mango.com/US/p0/women/clothing/coats/coats/check-wool-blend-coat/?id=73095535_70&n=1&s=prendas.abrigos');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (24, 2, 22, 6, 'Metal ring set', 'REF. 73040138 - RENNES', 'http://shop.mango.com/US/p0/women/accessories/jewellery/rings/metal-ring-set/?id=73040138_OR&n=1&s=accesorios.bisuteria');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (12, 2, 23, 6, 'Fringed cross strap sandals', 'REF. 73060083 - FATIMA-R', 'http://shop.mango.com/US/p0/women/accessories/shoes/heeled-sandals/fringed-cross-strap-sandals/?id=73060083_99&n=0&ts=1473153415629');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (3, 2, 24, 6, 'Zip skirt', 'REF. 73085594 - CREMI', 'http://shop.mango.com/US/p0/women/clothing/skirts/mini/zip-skirt/?id=73085594_99&n=1&s=prendas.faldas');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (4, 2, 25, 6, 'Embroidered blouse', 'REF. 73025581 - TORY', 'http://shop.mango.com/US/p0/women/clothing/shirts/blouses/embroidered-blouse/?id=73025581_02&n=1&s=prendas.blusas');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (4, 2, 26, 6, 'Zip blouse', 'REF. 71035620 - SOMBRA', 'http://shop.mango.com/US/p0/women/clothing/shirts/blouses/zipped-blouse/?id=71035620_91&n=1&s=prendas.blusas');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (4, 2, 27, 6, 'Tie-neck blouse', 'REF. 71045544 - DOLO', 'http://shop.mango.com/US/p0/women/clothing/shirts/blouses/tie-neck-blouse/?id=71045544_57&n=1&s=prendas.blusas');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (4, 2, 28, 6, 'Check cotton shirt', 'REF. 73073511 - COLDPLAY', 'http://shop.mango.com/US/p0/women/clothing/shirts/shirts/check-cotton-shirt/?id=73073511_70&n=1&s=prendas.blusas');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (11, 2, 29, 6, 'Leather chelsea ankle boots', 'REF. 73085560 - CHEALSEA3', 'http://shop.mango.com/AU/p0/woman/accessories/shoes/boots-and-ankle-boots/leather-chelsea-ankle-boots/?id=73085560_99&n=1&s=accesorios.zapatos');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (14, 2, 30, 6, 'Interwoven cord sandas', 'REF. 73060085 - NUDOFLAT', 'http://shop.mango.com/AU/p0/woman/accessories/shoes/flat-sandals/interwoven-cord-sandals/?id=73060085_15&n=1&s=accesorios.zapatos');
+
+INSERT INTO clothing(id_subcategory, id_gender, id_picture, id_brand, name_clothing, reference_clothing, link_officialwebsite)
+VALUES (13, 2, 31, 6, 'Pointed toe flat shoes', 'REF. 73093524 - BOL', 'http://shop.mango.com/AU/p0/woman/accessories/shoes/flat-shoes/pointed-toe-flat-shoes/?id=73093524_99&n=1&s=accesorios.zapatos');
+
+
+/* CLOTHING_SEASONS */
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 13);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 13);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 13);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 13);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 14);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 14);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 14);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 14);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 15);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 15);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 15);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 15);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 16);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 16);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 16);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 16);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 17);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 17);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 17);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 18);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 18);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 18);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 18);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 19);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 19);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 19);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 20);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 20);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 20);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 20);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 21);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 21);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 21);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 22);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 22);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 22);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 22);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 23);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 23);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 23);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 23);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 24);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 24);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 24);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 24);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 25);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 25);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 25);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 26);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 26);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 26);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 27);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 27);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 27);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 28);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 28);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 28);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 28);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 29);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 29);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 30);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 30);
+
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (1, 31);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (2, 31);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (3, 31);
+INSERT INTO clothing_seasons(id_season, id_clothing) VALUES (4, 31);
+
+/* CLOTHES */
+INSERT INTO clothes(id_clothing, id_dressing) VALUES (13, 1);
+INSERT INTO clothes(id_clothing, id_dressing) VALUES (17, 1);
+INSERT INTO clothes(id_clothing, id_dressing) VALUES (20, 1);
